e377ef13da0bccf8b7ca3f75939d48ba15bf4a98
BenStrawbridge <lute_midnight@yahoo.com>
2020-11-02 14:42:41 -0600

    Classes Exercise 1

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car focus = new Car();
+            focus.Make = "Ford";
+            focus.Model = "Focus";
+            focus.Year = 2014;
+
+            Console.WriteLine("The make is: " + focus.Make);
+            Console.WriteLine("The model is: " + focus.Model);
+            Console.WriteLine("The year is: " + focus.Year);
+        }
+    }
+}
